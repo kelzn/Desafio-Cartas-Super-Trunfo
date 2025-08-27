5d921383c9a65c5a18a98e79fa245d17352a2a8e
kelzn <kelvin.cassiano33@gmail.com>
2025-08-27 20:45:04 -0300

    Solução criada

---
+++ N1_NC4/n1_nc4_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    //Sintaxe padrão printf
+    printf("Olá Mundo!");
+    //printf("texto com formação", variavel1, variavel2, ...);
+    //printf("Ola Munso!\n");
+    
+}
